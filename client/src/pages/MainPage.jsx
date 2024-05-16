0849a2f19979e31c2e2d21db9bf61b317986ba19
jmattftan <jamesmatthewftan@gmail.com>
2024-05-17 01:09:53 +0800

    Added a Loading Page (LoadingPage.jsx).

--- client/src/pages/MainPage.jsx
+++ client/src/pages/MainPage.jsx
@@ -2,12 +2,20 @@ import React, { useState, useEffect } from "react";
 import ExitButton from "../components/ExitButton";
 import TrailMap from "../components/TrailMap";
 import BottomSection from "../components/BottomSection";
-import ReturnToLoginPage from "../components/ReturnToLoginPage";
+import ReturnToLoginPage from "./ReturnToLoginPage";
+import LoadingPage from "./LoadingPage";
 
 const MainPage = ({ socket }) => {
   const [authentication, setAuthentication] = useState(null);
+  const [isServerConnected, setIsServerConnected] = useState(socket.connected);
 
   useEffect(() => {
+    if (socket.connected) {
+      setIsServerConnected(true);
+    } else {
+      setIsServerConnected(false);
+    }
+
     socket.on("users", (users) => {
       const authenticatedUser = users.find((user) => user.id === socket.id);
       if (authenticatedUser) {
@@ -20,6 +28,10 @@ const MainPage = ({ socket }) => {
     };
   }, [socket]);
 
+  if (!isServerConnected) {
+    return <ReturnToLoginPage />;
+  }
+
   return (
     <>
       {authentication?.id &&
@@ -31,7 +43,7 @@ const MainPage = ({ socket }) => {
           <BottomSection socket={socket} />
         </div>
       ) : (
-        <ReturnToLoginPage />
+        <LoadingPage />
       )}
     </>
   );
--- client/src/pages/ReturnToLoginPage.jsx
+++ client/src/pages/ReturnToLoginPage.jsx
@@ -11,7 +11,7 @@ const ReturnToLoginPage = () => {
   return (
     <div className="bg-green-800 justify-center flex flex-col items-center h-screen gap-5 pt-3 pb-3 pl-8 pr-8 lg:pl-10 lg:pr-10">
       <p className="text-xl text-center text-slate-200 font-normal tracking-tight leading-loose">
-        You've been Disconnected, return to Login page.
+        YOU'VE BEEN DISCONNECTED, RETURN TO LOGIN PAGE.
       </p>
       <button
         className="text-slate-700 hover:text-slate-200 bg-slate-200 hover:bg-slate-900 shadow-lg rounded-md border-2 border-slate-300 justify-center flex flex-row items-center gap-1.5 px-5 py-1.5 transition-all duration-300"
